412a341e806110a4e584f5044ea97a620ec4587c
yrikitake <yoshiaki@sendai-nct.ac.jp>
2022-01-02 18:45:39 +0900

    parallel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use structopt::StructOpt;
+use rayon::prelude::*;
 
 #[derive(StructOpt)]
 #[structopt(name = "rsgrep")]
@@ -21,7 +22,7 @@ fn grep(state: &GrepArgs, content: String, file_name: &str) {
 fn run(state: GrepArgs) {
     state
         .path
-        .iter()
+        .par_iter()
         .for_each(|file| match read_to_string(file) {
             Ok(content) => grep(&state, content, &file),
             Err(reason) => println!("{}", reason),
